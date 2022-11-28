4756f0bbb182d0e96473e197b4837d00536317f7
vybz.live <73053667+zakirhossen23@users.noreply.github.com>
2022-11-28 10:44:54 +0600

    categories

--- wavedata-api/pages/api/GET/Trial/Survey/GetSurveyDetails.js
+++ wavedata-api/pages/api/GET/Trial/Survey/GetSurveyDetails.js
@@ -17,19 +17,29 @@ export default async function handler(req, res) {
 
   let survey_element = await contract._surveyMap(req.query.surveyid).call();
   var new_survey = {
-     id: Number(survey_element.survey_id),
-     trial_id: Number(survey_element.trial_id),
-     user_id: Number(survey_element.user_id),
-     name: survey_element.name,
-     description: survey_element.description,
-     date: survey_element.date,
-     image: survey_element.image,
-     reward: Number(survey_element.reward),
-     submission: Number(survey_element?.submission)
+    id: Number(survey_element.survey_id),
+    trial_id: Number(survey_element.trial_id),
+    user_id: Number(survey_element.user_id),
+    name: survey_element.name,
+    description: survey_element.description,
+    date: survey_element.date,
+    image: survey_element.image,
+    reward: Number(survey_element.reward),
+    submission: Number(survey_element?.submission)
   };
-  let final={
+  let allCategory = [];
+  for (let i = 0; i < Number(await contract._SurveyCategoryIds().call()); i++) {
+    const element = await contract._categoryMap(i).call();
+    allCategory.push({
+      name: element.name,
+      image: element.image
+    })
+  }
+
+  let final = {
     Survey: new_survey,
-    Sections: JSON.parse(await contract._sectionsMap(req.query.surveyid).call())
+    Sections: JSON.parse(await contract._sectionsMap(req.query.surveyid).call()),
+    Categories: allCategory
   }
 
   res.status(200).json({ status: 200, value: final })
